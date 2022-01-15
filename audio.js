86efbd1981d53dd0150a8525d89b6fcbcb931582
Thomas <tomahawktothedome@gmail.com>
2022-01-15 11:40:41 -0500

    can play from youtube

--- audio.js
+++ audio.js
@@ -3,11 +3,14 @@ const DiscordVoice = require('@discordjs/voice')
 const { joinVoiceChannel, getVoiceConnection } = require('@discordjs/voice')
 const speech = require('@google-cloud/speech');
 const fs = require('fs');
+const ytdl = require('ytdl-core');
 
 
 const client = new Discord.Client({intents: ["GUILD_MESSAGES", "GUILD_VOICE_STATES", "GUILDS"]});
 const speechClient = new speech.SpeechClient(); 
 const config = require("./config.json");
+const player = DiscordVoice.createAudioPlayer();
+const listener = new DiscordVoice.VoiceReceiver();
 
 
 
@@ -44,10 +47,6 @@ client.on("message", async message => {
             selfDeaf: false,
         });
         const connection2 = getVoiceConnection(channel.guild.id);
-        const connection3 = new AudioReceiveStream()
-        console.log(connection3)
-
-        console.log(connection3.speaking)
         
         connection2.on('voiceStateUpdate', (user, speaking) => { 
             console.log("THSIASIHORHWVOAEUVHLSIDGH,SDFKS")
@@ -91,6 +90,27 @@ client.on("message", async message => {
 
         });
     }
+    if(command === "play"){
+        if ((message.member.voice.channel.members.filter((e) => client.user.id === e.user.id).size == 0)) return message.reply(`I'm not in a channel`);
+        const resource = DiscordVoice.createAudioResource('track.mp3');
+        player.play(resource);
+        const connection = getVoiceConnection(channel.guild.id);
+
+        // Play "track.mp3" across two voice connections
+        connection.subscribe(player);
+    }
+
+    if(command === "play1"){
+        if ((message.member.voice.channel.members.filter((e) => client.user.id === e.user.id).size == 0)) return message.reply(`I'm not in a channel`);
+        const url = args[0];
+
+        if(!url) return message.reply('No url');
+        const stream = ytdl(url, {filter: 'audioonly'});
+        const resource = DiscordVoice.createAudioResource(stream);
+
+        player.play(resource)
+    }
+
     if(command === "leave") {
         if ((message.member.voice.channel.members.filter((e) => client.user.id === e.user.id).size == 0)) return message.reply(`I already left`);
         const connection = getVoiceConnection(channel.guild.id);
