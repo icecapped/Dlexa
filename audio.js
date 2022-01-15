8535564599ac5bc1a51778ed5d14367ddbb2df15
Thomas <tomahawktothedome@gmail.com>
2022-01-15 09:22:16 -0500

    js

--- audio.js
+++ audio.js
@@ -1,15 +1,19 @@
 const Discord = require("discord.js");
+const DiscordVoice = require('@discordjs/voice')
+const { joinVoiceChannel, getVoiceConnection } = require('@discordjs/voice')
 const speech = require('@google-cloud/speech');
 const fs = require('fs');
+
 
 const client = new Discord.Client({intents: ["GUILD_MESSAGES", "GUILD_VOICE_STATES", "GUILDS"]});
 const speechClient = new speech.SpeechClient(); 
 const config = require("./config.json");
 
 
+
 client.on("ready", () => {
     console.log(`Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
-    client.user.setActivity(`Serving ${client.guilds.cache.size} servers`);
+    client.user.setActivity(`Farting on all of humanity`);
 });
 
 client.on("message", async message => {
@@ -19,12 +23,13 @@ client.on("message", async message => {
   
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
-     
+    const channel = message.member.voice.channel; 
+ 
     if(command === "ping") {
         const m = await message.channel.send("Ping?");
         m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
     }
-
+    
     if(command === "join") {
         if (!message.member.voice.channel) return message.reply('Please join a voice channel first!');
         if ((message.member.voice.channel.members.filter((e) => client.user.id === e.user.id).size > 0)) return message.reply(`I'm already in your voice channel!`);
@@ -32,47 +37,64 @@ client.on("message", async message => {
         if (!message.member.voice.channel.joinable) return message.reply(`I don't have permission to join that voice channel!`);
         if (!message.member.voice.channel.speakable) return message.reply(`I don't have permission to speak in that voice channel!`);
 
-        const connection = await message.member.voice.channel.join(); 
-        await connection.play('ding.wav');
+        const connection = joinVoiceChannel({
+            channelId: channel.id,
+            guildId: channel.guild.id,
+            adapterCreator: channel.guild.voiceAdapterCreator,
+            selfDeaf: false,
+        });
+        const connection2 = getVoiceConnection(channel.guild.id);
+        const connection3 = new AudioReceiveStream()
+        console.log(connection3)
 
-        connection.on('speaking', (user, speaking) => { 
+        console.log(connection3.speaking)
+        
+        connection2.on('voiceStateUpdate', (user, speaking) => { 
+            console.log("THSIASIHORHWVOAEUVHLSIDGH,SDFKS")
             if (user.bot) return; 
-            if (!speaking) return; 
+            if (!speaking) return;
+            console.log(user);
 
-            const audio = connection.receiver.createStream(user, { mode: 'pcm' }); 
 
-            const audioFileName = './recordings/' + user.id + '_' + Date.now() + '.pcm';
+            // const audio = connection.receiver.createStream(user, { mode: 'pcm' }); 
 
-            audio.pipe(fs.createWriteStream(audioFileName));
+            // const audioFileName = './recordings/' + user.id + '_' + Date.now() + '.pcm';
+
+            // audio.pipe(fs.createWriteStream(audioFileName));
             
-            audio.on('end', async () => {
-                fs.stat(audioFileName, async (err, stat) => { 
-                    if (!err && stat.size) {
-                        const file = fs.readFileSync(audioFileName);
-                        const audioBytes = file.toString('base64');
-                        const audio = {
-                        content: audioBytes,
-                    };
-                    const config = {
-                        encoding: 'LINEAR16',
-                        sampleRateHertz: 48000,
-                        languageCode: 'en-US',
-                        audioChannelCount: 2,
-                    };
-                    const request = {
-                        audio: audio,
-                        config: config,
-                    };
-                    const [response] = await speechClient.recognize(request);
-                    const transcription = response.results
-                        .map(result => result.alternatives[0].transcript)
-                        .join('\n');
-                    message.reply(transcription);
-                }
-                });
-            });
+            // audio.on('end', async () => {
+            //     fs.stat(audioFileName, async (err, stat) => { 
+            //         if (!err && stat.size) {
+            //             const file = fs.readFileSync(audioFileName);
+            //             const audioBytes = file.toString('base64');
+            //             const audio = {
+            //             content: audioBytes,
+            //         };
+            //         const config = {
+            //             encoding: 'LINEAR16',
+            //             sampleRateHertz: 48000,
+            //             languageCode: 'en-US',
+            //             audioChannelCount: 2,
+            //         };
+            //         const request = {
+            //             audio: audio,
+            //             config: config,
+            //         };
+            //         const [response] = await speechClient.recognize(request);
+            //         const transcription = response.results
+            //             .map(result => result.alternatives[0].transcript)
+            //             .join('\n');
+            //         message.reply(transcription);
+            //     }
+            //     });
+            // });
 
         });
+    }
+    if(command === "leave") {
+        if ((message.member.voice.channel.members.filter((e) => client.user.id === e.user.id).size == 0)) return message.reply(`I already left`);
+        const connection = getVoiceConnection(channel.guild.id);
+        connection.destroy();        
     }
   
 });
