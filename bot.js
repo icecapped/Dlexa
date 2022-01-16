9496edfe963ceee11af9022888d3210c7f39e154
Thomas <tomahawktothedome@gmail.com>
2022-01-16 10:21:59 -0500

    tts playing

--- bot.js
+++ bot.js
@@ -24,7 +24,6 @@ client.on("ready", () => {
 });
 
 client.on("message", async message => {
-    if(message.author.bot) return; 
     if(!message.content.startsWith(config.prefix)) return;
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
@@ -39,7 +38,7 @@ client.on("message", async message => {
     }
     
     //use utils
-    utils.respondToMessage(message);
+    utils.respondToMessage(message, client);
     if(command === "ping") {
         const m = await message.channel.send("Ping?");
         m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
